580a8ba780107de190960d77b1d588ac522bb3e4
beir3200 <BeierSylvester@outlook.com>
2024-06-11 05:29:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ function App() {
 
   return (
     <div className="App">
-      <h1 style={{color: "#fff"}}>Blckclov3r</h1>
-      <h2 style={{color: "#fff"}}>Blckclov3r</h2>
-      <h3 style={{color: "#fff"}}>Blckclov3r</h3>
-      <h4 style={{color: "#fff"}}>Blckclov3r</h4>
-      <h5 style={{color: "#fff"}}>Blckclov3r</h5>
-      <h6 style={{color: "#fff"}}>Blckclov3r</h6>
+      <h1 style={{color: "#fff"}}>Blckclov</h1>
+      <h2 style={{color: "#fff"}}>Blckclov</h2>
+      <h3 style={{color: "#fff"}}>Blckclov</h3>
+      <h4 style={{color: "#fff"}}>Blckclov</h4>
+      <h5 style={{color: "#fff"}}>Blckclov</h5>
+      <h6 style={{color: "#fff"}}>Blckclov</h6>
       <h1 style={{color: "#fff"}}>Aljun</h1>
       <h2 style={{color: "#fff"}}>Aljun</h2>
       <h3 style={{color: "#fff"}}>Aljun</h3>
